6a9b59c288cdc44f173bb2ab7d574dae7a945f67
Aymane-Elhassouni <aymane.elhassouni3@gmail.com>
2025-11-19 12:07:37 +0100

    first edite

---
+++ script1.js
@@ -0,0 +1,6 @@
+const element = document.getElementById("userAge");
+
+document.getElementById("cancelBtn").addEventListener(("click","element"), function(event){
+    event.console.log(element);
+    
+})
